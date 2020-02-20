1ab922e3352f88d84d74490f2a081b280c2fa59b
Minoru Osuka <minoru.osuka@gmail.com>
2020-02-20 14:56:17 +0900

    Change word details to vec from struct (#1)

--- README.md
+++ README.md
@@ -33,16 +33,37 @@ Building a dictionary with `lindera-ko-dic` command:
 % ./bin/lindera-ko-dic ./mecab-ko-dic-2.1.1-20180720 ./lindera-ko-dic-2.1.1-20180720
 ```
 
+## Dictionary format
+
+Information about the dictionary format and part-of-speech tags used by mecab-ko-dic id documented in [this Google Spreadsheet](https://docs.google.com/spreadsheets/d/1-9blXKjtjeKZqsf4NzHeYJCrr49-nXeRF6D80udfcwY/edit#gid=589544265), linked to from mecab-ko-dic's [repository readme](https://bitbucket.org/eunjeon/mecab-ko-dic/src/master/README.md).
+
+Note how ko-dic has one less feature column than NAIST JDIC, and has an altogether different set of information (e.g. doesn't provide the "original form" of the word).
+
+The tags are a slight modification of those specified by 세종 (Sejong), whatever that is. The mappings from Sejong to mecab-ko-dic's tag names are given in tab `태그 v2.0` on the above-linked spreadsheet.
+
+The dictionary format is specified fully (in Korean) in tab `사전 형식 v2.0` of the spreadsheet. Any blank values default to `*`.
+
+| Index | Name (Korean) | Name (English) | Notes |
+| --- | --- | --- | --- |
+| 0 | 품사 태그 | part-of-speech tag | See `태그 v2.0` tab on spreadsheet  |
+| 1 | 의미 부류 | meaning | (too few examples for me to be sure) |
+| 2 | 종성 유무 | presence or absence | `T` for true; `F` for false; else `*` |
+| 3 | 읽기 | reading | usually matches surface, but may differ for foreign words e.g. Chinese character words |
+| 4 | 타입 | type | One of: `Inflect` (활용); `Compound` (복합명사); or `Preanalysis` (기분석) |
+| 5 | 첫번째 품사 | first part-of-speech | e.g. given a part-of-speech tag of "VV+EM+VX+EP", would return `VV` |
+| 6 | 마지막 품사 | last part-of-speech | e.g. given a part-of-speech tag of "VV+EM+VX+EP", would return `EP` |
+| 7 | 표현 | expression | `활용, 복합명사, 기분석이 어떻게 구성되는지 알려주는 필드` – Fields that tell how usage, compound nouns, and key analysis are organized |
+
 ## Tokenizing text using produced dictionary
 
 You can tokenize text using produced dictionary with `lindera` command:
 
 ```
 % echo "하네다공항한정토트백" | lindera -d ./lindera-ko-dic-2.1.1-20180720
-하네다  NNP,인명,F,하네다,*,*,*,*,
-공항    NNG,장소,T,공항,*,*,*,*,
-한정    NNG,*,T,한정,*,*,*,*,
-토트백  NNG,*,T,토트백,Compound,*,*,토트/NNP/인명+백/NNG/*,
+하네다  NNP,인명,F,하네다,*,*,*,*
+공항    NNG,장소,T,공항,*,*,*,*
+한정    NNG,*,T,한정,*,*,*,*
+토트백  NNG,*,T,토트백,Compound,*,*,토트/NNP/인명+백/NNG/*
 EOS
 ```
 
--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ use std::{fs, u32};
 use bincode;
 use byteorder::ByteOrder;
 use byteorder::{LittleEndian, WriteBytesExt};
+use csv::StringRecord;
 use encoding::all::UTF_16LE;
 use encoding::{DecoderTrap, Encoding};
 use glob::glob;
@@ -17,9 +18,8 @@ use lindera_core::core::character_definition::{
     CategoryData, CategoryId, CharacterDefinitions, LookupTable,
 };
 use lindera_core::core::unknown_dictionary::UnknownDictionary;
-use lindera_core::core::word_entry::{WordDetail, WordEntry, WordId};
+use lindera_core::core::word_entry::{WordEntry, WordId};
 use lindera_fst::MapBuilder;
-use csv::StringRecord;
 
 #[derive(Debug)]
 pub enum ParsingError {
@@ -43,50 +43,6 @@ impl From<io::Error> for ParsingError {
 impl From<ParseIntError> for ParsingError {
     fn from(parse_err: ParseIntError) -> Self {
         ParsingError::from_error(parse_err)
-    }
-}
-
-#[derive(Debug)]
-pub struct CSVRow {
-    surface_form: String,
-    left_id: u32,
-    right_id: u32,
-    word_cost: i32,
-
-    pos_level1: String,
-    pos_level2: String,
-    pos_level3: String,
-    pos_level4: String,
-
-    pub conjugation_type: String,
-    pub conjugate_form: String,
-
-    pub base_form: String,
-    pub reading: String,
-    pronunciation: String,
-}
-
-impl CSVRow {
-    fn from_record(record: &StringRecord) -> CSVRow {
-        println!("{:?}", record);
-        CSVRow {
-            surface_form: record[0].trim().to_string(),
-            left_id: u32::from_str(&record[1]).expect("failed to parse left_id"),
-            right_id: u32::from_str(&record[2]).expect("failed to parse right_id"),
-            word_cost: i32::from_str(&record[3]).expect("failed to parse word_cost"),
-
-            pos_level1: record[4].to_string(),
-            pos_level2: record[5].to_string(),
-            pos_level3: record[6].to_string(),
-            pos_level4: record[7].to_string(),
-
-            conjugation_type: record[8].to_string(),
-            conjugate_form: record[9].to_string(),
-
-            base_form: record[10].to_string(),
-            reading: record[11].to_string(),
-            pronunciation: "".to_string(),
-        }
     }
 }
 
@@ -114,69 +70,62 @@ fn build_dict(input_dir: &str, output_dir: &str) -> Result<(), ParsingError> {
         }
     }
 
-    let mut rows: Vec<CSVRow> = Vec::new();
+    let mut rows: Vec<StringRecord> = Vec::new();
     for filename in filenames {
-        let mut rdr = csv::ReaderBuilder::new().has_headers(false).from_path(Path::new(input_dir).join(Path::new(filename.as_str()))).unwrap();
+        println!("reading csv: {}", filename);
+
+        let mut rdr = csv::ReaderBuilder::new()
+            .has_headers(false)
+            .from_path(Path::new(input_dir).join(Path::new(filename.as_str())))
+            .unwrap();
+
         for result in rdr.records() {
             let record = result.unwrap();
-
-            let csv_row = CSVRow::from_record(&record);
-
-            rows.push(csv_row);
+            rows.push(record);
         }
     }
 
-    println!("  - parsed csv");
-    rows.sort_by_key(|row| row.surface_form.clone());
-//    rows.sort_by_key(|row| row.base_form.clone());
-    println!("  - sorted csv");
+    println!("sorting entries");
+    rows.sort_by_key(|row| row[0].to_string().clone());
 
-    let wtr_fst = io::BufWriter::new(File::create(
-        Path::new(output_dir).join(Path::new("dict.fst")),
-    )?);
-    let mut wtr_vals = io::BufWriter::new(File::create(
-        Path::new(output_dir).join(Path::new("dict.vals")),
-    )?);
+    let wtr_fst = io::BufWriter::new(
+        File::create(Path::new(output_dir).join(Path::new("dict.fst"))).unwrap(),
+    );
+    let mut wtr_vals = io::BufWriter::new(
+        File::create(Path::new(output_dir).join(Path::new("dict.vals"))).unwrap(),
+    );
 
     let mut word_entry_map: BTreeMap<String, Vec<WordEntry>> = BTreeMap::new();
 
     for (row_id, row) in rows.iter().enumerate() {
-        if row.word_cost == 3978 {
-            println!(
-                "{} -> {}",
-                row.surface_form,
-                row.surface_form.chars().next().unwrap() as u32
-            );
-        }
         word_entry_map
-            .entry(row.surface_form.to_string())
+            .entry(row[0].to_string())
             .or_insert_with(Vec::new)
             .push(WordEntry {
                 word_id: WordId(row_id as u32),
-                word_cost: row.word_cost as i16,
-                cost_id: row.left_id as u16,
+                word_cost: i16::from_str(row[3].trim()).unwrap(),
+                cost_id: u16::from_str(row[1].trim()).unwrap(),
             });
     }
 
-    let mut wtr_words = io::BufWriter::new(File::create(
-        Path::new(output_dir).join(Path::new("dict.words")),
-    )?);
-    let mut wtr_words_idx = io::BufWriter::new(File::create(
-        Path::new(output_dir).join(Path::new("dict.wordsidx")),
-    )?);
+    let mut wtr_words = io::BufWriter::new(
+        File::create(Path::new(output_dir).join(Path::new("dict.words"))).unwrap(),
+    );
+    let mut wtr_words_idx = io::BufWriter::new(
+        File::create(Path::new(output_dir).join(Path::new("dict.wordsidx"))).unwrap(),
+    );
     let mut words_buffer = Vec::new();
     for row in rows.iter() {
-        let word = WordDetail {
-            pos_level1: row.pos_level1.to_string(),
-            pos_level2: row.pos_level2.to_string(),
-            pos_level3: row.pos_level3.to_string(),
-            pos_level4: row.pos_level4.to_string(),
-            conjugation_type: row.conjugation_type.to_string(),
-            conjugate_form: row.conjugate_form.to_string(),
-            base_form: row.base_form.to_string(),
-            reading: row.reading.to_string(),
-            pronunciation: row.pronunciation.to_string(),
-        };
+        let word = vec![
+            row[4].to_string(),
+            row[5].to_string(),
+            row[6].to_string(),
+            row[7].to_string(),
+            row[8].to_string(),
+            row[9].to_string(),
+            row[10].to_string(),
+            row[11].to_string(),
+        ];
         let offset = words_buffer.len();
         wtr_words_idx.write_u32::<LittleEndian>(offset as u32)?;
         bincode::serialize_into(&mut words_buffer, &word).unwrap();
@@ -188,24 +137,24 @@ fn build_dict(input_dir: &str, output_dir: &str) -> Result<(), ParsingError> {
 
     let mut id = 0u64;
 
-    println!("  - building fst");
+    println!("building fst");
     let mut fst_build = MapBuilder::new(wtr_fst).unwrap();
     for (key, word_entries) in &word_entry_map {
         let len = word_entries.len() as u64;
-//        assert!(len < (1 << 5));
         let val = (id << 5) | len;
         fst_build.insert(&key, val).unwrap();
         id += len;
     }
     fst_build.finish().unwrap();
-    println!("  - built fst");
+
+    println!("building values");
     for word_entries in word_entry_map.values() {
         for word_entry in word_entries {
             word_entry.serialize(&mut wtr_vals)?;
         }
     }
-    wtr_vals.flush()?;
-    println!(" - built values");
+    wtr_vals.flush().unwrap();
+
     Ok(())
 }
 
